3723968999cf24f3c2f4cef63e671ca33f1de062
UNASP-SP - Alexandre Valdinei Vigani <alexandre.vigani@unasp.edu.br>
2023-11-29 22:28:12 -0300

    Primeira execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello, World!")
+})
+
+app.listen(3000, () =>{
+    console.log("servidor rodando na porta 3000!")
+})
