9738cc6275eec772954cbf3f234cc67f0d912abb
UNASP-SP - Alexandre Valdinei Vigani <alexandre.vigani@unasp.edu.br>
2023-11-29 22:38:59 -0300

    instalação do hanlebars e do nodemon

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const exphbs = require("expresss-handlebars")
 
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
     res.send("Hello, World!")
